5f721d6ca27bc91332be26382bd62b0737d93cdd
杜凯希 <cdube85@gmail.com>
2017-04-22 13:31:33 -0700

    Startup and Shutdown

---
+++ Startup.py
@@ -0,0 +1,77 @@
+import os
+import time
+
+def openDropbox():
+    try:
+        os.startfile('C:\Program Files (x86)\Dropbox\Client\dropbox.exe')
+
+    except Exception as e: print(e)
+
+def closeDropbox():
+    try:
+        os.system('TASKKILL /F /IM Dropbox.exe')
+        
+    except Exception as e: print(e)
+
+def openBox():
+    try:
+        os.startfile('C:\Program Files\Box\Box Sync\BoxSync.exe')
+
+    except Exception as e: print(e)
+
+def closeBox():
+    try:
+        os.system('TASKKILL /F /IM BoxSync.exe')
+        
+    except Exception as e: print(e)
+
+def openOutlook():
+    try:
+        os.startfile('OUTLOOK.exe')
+
+    except Exception as e: print(e)
+
+def openJabber():
+    try:
+        os.startfile('C:\Program Files (x86)\Cisco Systems\Cisco Jabber\CiscoJabber.exe')
+
+    except Exception as e: print(e)
+   
+def openVPN():
+    try:
+        os.startfile('C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\\vpnui.exe')
+
+    except Exception as e: print(e)
+
+def openChrome():
+    try:
+        os.startfile('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
+
+    except Exception as e: print(e)
+
+
+
+openVPN()
+time.sleep(30)
+
+openOutlook()
+time.sleep(5)
+
+openChrome()
+time.sleep(3)
+
+openDropbox()
+openBox()
+time.sleep(5)
+
+openJabber()
+time.sleep(180)
+
+closeDropbox()
+time.sleep(2)
+
+closeBox()
+
+exit()
+
+
